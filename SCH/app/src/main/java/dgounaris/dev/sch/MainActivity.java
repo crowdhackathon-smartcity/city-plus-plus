1b4a28e8f613d8c279e7376106cbf161e0ab8626
AntonisKl <denti20082008@hotmail.com>
2017-05-13 10:30:33 +0300

    default_fragment_on_start

--- SCH/app/src/main/java/dgounaris/dev/sch/MainActivity.java
+++ SCH/app/src/main/java/dgounaris/dev/sch/MainActivity.java
@@ -46,6 +46,11 @@ public class MainActivity extends AppCompatActivity {
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // default fragment
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.framelayout, new home_fragment());
+        transaction.commit();
     }
 
 }
