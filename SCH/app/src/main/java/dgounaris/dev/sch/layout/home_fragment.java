ffd9da06d95b6d9ad1aee183e744604bbd252b0a
AntonisKl <denti20082008@hotmail.com>
2017-05-13 16:13:19 +0300

    fixing popupwindow

--- SCH/app/src/main/java/dgounaris/dev/sch/layout/home_fragment.java
+++ SCH/app/src/main/java/dgounaris/dev/sch/layout/home_fragment.java
@@ -4,9 +4,16 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import dgounaris.dev.sch.People.Person;
 import dgounaris.dev.sch.R;
@@ -14,6 +21,10 @@ import dgounaris.dev.sch.R;
 public class home_fragment extends Fragment {
 
     private Person activeperson;
+
+    PopupWindow popUpWindow;
+
+    LinearLayout.LayoutParams layoutParams;
 
     public home_fragment() {
         // Required empty public constructor
@@ -34,7 +45,40 @@ public class home_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_fragment, container, false);
+        final View view = inflater.inflate(R.layout.fragment_home_fragment, container, false);
+
+        final ImageButton button = (ImageButton) view.findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popUpWindow = new PopupWindow();
+                popUpWindow.showAtLocation(getView(), Gravity.BOTTOM, 10, 10);
+                popUpWindow.update(50, 50, 320, 90);
+                TextView msg = new TextView(getContext());
+                msg.setText("Connecting...");
+
+                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+                        LinearLayout.LayoutParams.WRAP_CONTENT);
+
+                LinearLayout containerLayout = new LinearLayout(getContext());
+
+                containerLayout.setOrientation(LinearLayout.VERTICAL);
+                containerLayout.addView(msg, layoutParams);
+                popUpWindow.setContentView(containerLayout);
+                //FrameLayout mainLayout = (FrameLayout) view.findViewById(R.id.frame);
+
+
+
+                //mainLayout.removeAllViewsInLayout();
+                //mainLayout.addView(button, layoutParams);
+
+            }
+        });
+
+
+        return view;
+
     }
 
     private void onBinConnection() {
