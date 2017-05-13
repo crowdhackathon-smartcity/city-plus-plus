98ed69113538f9a658c57ce6ef3817cb95e590de
Dimitris Gounaris <dimitris.gounaris@hotmail.com>
2017-05-13 18:48:37 +0300

    Add files via upload

--- SCH/app/src/main/java/dgounaris/dev/sch/DBHelper/MyDBContract.java
+++ SCH/app/src/main/java/dgounaris/dev/sch/DBHelper/MyDBContract.java
@@ -8,15 +8,46 @@ import android.provider.BaseColumns;
 
 final public class MyDBContract {
 
-        //make it private so that nobody can initialize this
-        private MyDBContract() {}
+    //make it private so that nobody can initialize this
+    private MyDBContract() {
+    }
 
-        public static class People implements BaseColumns {
-            public static final String TABLE_NAME = "People";
-            public static final String COLUMN_NAME_ID = "Id";
-            public static final String COLUMN_NAME_NAME = "Name";
-            public static final String COLUMN_NAME_SURNAME = "Surname";
-            public static final String COLUMN_NAME_IMAGE = "Image";
-        }
+    public static class People implements BaseColumns {
+        public static final String TABLE_NAME = "People";
+        public static final String COLUMN_NAME_ID = "Id";
+        public static final String COLUMN_NAME_NAME = "Name";
+        public static final String COLUMN_NAME_SURNAME = "Surname";
+        public static final String COLUMN_NAME_IMAGE = "Image";
+        public static final String COLUMN_NAME_POINTS = "Points";
+    }
+
+    public static class Credentials implements BaseColumns {
+        public static final String TABLE_NAME = "Credentials";
+        public static final String COLUMN_NAME_USERNAME = "Username";
+        public static final String COLUMN_NAME_PASSWORD = "Password";
+        public static final String COLUMN_NAME_PERSON_ID = "Person_Id";
+    }
+
+    public static class Trophies implements BaseColumns {
+        public static final String TABLE_NAME = "Trophies";
+        public static final String COLUMN_NAME_ID = "Id";
+        public static final String COLUMN_NAME_NAME = "Name";
+        public static final String COLUMN_NAME_DESCRIPTION = "Description";
+        public static final String COLUMN_NAME_IMAGE = "Image";
+    }
+
+    public static class People_Trophies implements BaseColumns {
+        public static final String TABLE_NAME = "People_Trophies";
+        public static final String COLUMN_NAME_PERSON_ID = "Person_Id";
+        public static final String COLUMN_NAME_TROPHY_ID = "Trophy_id";
+    }
+
+    public static class Services implements BaseColumns {
+        public static final String TABLE_NAME = "Services";
+        public static final String COLUMN_NAME_ID = "Id";
+        public static final String COLUMN_NAME_NAME = "Name";
+        public static final String COLUMN_NAME_EMPTY_SLOTS = "EmptySlots";
+        public static final String COLUMN_NAME_POINTS = "Points";
+    }
 
 }
--- SCH/app/src/main/java/dgounaris/dev/sch/layout/profile_fragment.java
+++ SCH/app/src/main/java/dgounaris/dev/sch/layout/profile_fragment.java
@@ -1,18 +1,28 @@
 package dgounaris.dev.sch.layout;
 
+import android.app.Dialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
+import dgounaris.dev.sch.MainActivity;
 import dgounaris.dev.sch.People.Person;
+import dgounaris.dev.sch.People.Service;
 import dgounaris.dev.sch.R;
+import dgounaris.dev.sch.adapter.ServiceAdapter;
 import dgounaris.dev.sch.adapter.Trophy;
 import dgounaris.dev.sch.adapter.TrophyAdapter;
 
@@ -41,18 +51,15 @@ public class profile_fragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile_fragment, container, false);
 
-        //getActivity().setContentView(R.layout.word_list);
+        //SET NAME, BALANCE
+        TextView nameText = (TextView) view.findViewById(R.id.name);
+        nameText.setText(activeperson.getName() + " " + activeperson.getSurname());
+        TextView balance = (TextView) view.findViewById(R.id.balance);
+        balance.setText(activeperson.getPoints() + " points");
 
-        ArrayList<Trophy> trophies = new ArrayList<Trophy>();
-
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
-        trophies.add(new Trophy("bla", R.drawable.recycle2));
+        ArrayList<Trophy> trophies;
         // SET VALUES TO TROPHIES HERE
+        trophies = activeperson.getTrophies();
 
         TrophyAdapter adapter = new TrophyAdapter(getContext(), trophies);
 
@@ -61,6 +68,32 @@ public class profile_fragment extends Fragment {
 
         listView.setAdapter(adapter);
 
+        Button redeembutton = (Button) view.findViewById(R.id.redeem_button);
+        redeembutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showServices();
+            }
+        });
+
         return view;
     }
+
+    public void showServices() {
+        ArrayList<Service> services;
+        services = ((MainActivity)getActivity()).getAvailableServices();
+        if (services.isEmpty()) {
+            Toast.makeText(getContext(), "Sorry, no available redeeming options.", Toast.LENGTH_LONG).show();
+        }
+        else {
+            Dialog myDialog = new Dialog(getActivity());
+            myDialog.setContentView(R.layout.redeem_view);
+            ListView serviceList = (ListView) myDialog.findViewById(R.id.service_list);
+            serviceList.setAdapter(new ServiceAdapter(getContext(), services));
+            myDialog.setCancelable(true);
+            myDialog.setTitle("ListView");
+            myDialog.show();
+        }
+    }
+
 }
