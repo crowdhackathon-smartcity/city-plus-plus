0247c7bea7c1dcdbf18baaf211d517b1e8f01f66
Dimitris Gounaris <dimitris.gounaris@hotmail.com>
2017-05-13 16:08:53 +0300

    Add files via upload

---
+++ SCH/app/src/main/java/dgounaris/dev/sch/People/Service.java
@@ -0,0 +1,42 @@
+package dgounaris.dev.sch.People;
+
+/**
+ * Created by DimitrisLPC on 13/5/2017.
+ */
+
+public class Service {
+
+    private String name;
+    private int slots;
+    private int points_needed;
+
+    public Service(String name, int slots, int points_needed) {
+        this.name = name;
+        this.slots = slots;
+        this.points_needed = points_needed;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSlots(int slots) {
+        this.slots = slots;
+    }
+
+    public void setPoints_needed(int points_needed) {
+        this.points_needed = points_needed;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSlots() {
+        return slots;
+    }
+
+    public int getPoints_needed() {
+        return points_needed;
+    }
+}
